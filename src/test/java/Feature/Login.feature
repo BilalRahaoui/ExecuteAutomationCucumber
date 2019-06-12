390c826e4720f558d4b4a6fd69196e54dff23223
Bilal rahaoui <rahaoui.bilalma@gmail.com>
2019-06-12 17:04:23 +0100

    add scenarion outline to run our feature with multiple data

--- src/test/java/Feature/Login.feature
+++ src/test/java/Feature/Login.feature
@@ -1,9 +1,23 @@
 Feature: Login feature.
   Description : This feature will test login functionality for the given application "automationtesting.in"
-  Scenario: Login with valid username and password
+
+  #Scenario: Login with valid username and password
+    #Given Open browser
+    #When Enter the url "http://practice.automationtesting.in/"
+    #And Click on My Account link
+    #And Send registered username as "rahaoui.bilalma" and password as "Testing^^password@12456#"
+    #And Click on LOGIN button
+    #Then User must be successfully redirected to home page
+
+  Scenario Outline: Login with invalid data
     Given Open browser
     When Enter the url "http://practice.automationtesting.in/"
     And Click on My Account link
-    And Send registered username as "rahaoui.bilalma" and password as "Testing^^password@12456#"
+    And Send registered username as "<username>" and password as "<password>"
     And Click on LOGIN button
     Then User must be successfully redirected to home page
+    Examples:
+      | username        | password                 |
+      | rahaoui.bilalma | invalid.password         |
+      | invalid.email   | Testing^^password@12456# |
+      | invalid.email   | invalid.password         |
--- test-output/HTMLReport/report.js
+++ test-output/HTMLReport/report.js
@@ -4,10 +4,70 @@ formatter.feature({
   "description": "  Description : This feature will test login functionality for the given application \"automationtesting.in\"",
   "keyword": "Feature"
 });
+formatter.scenarioOutline({
+  "name": "Login with valid username and password",
+  "description": "",
+  "keyword": "Scenario Outline"
+});
+formatter.step({
+  "name": "Open browser",
+  "keyword": "Given "
+});
+formatter.step({
+  "name": "Enter the url \"http://practice.automationtesting.in/\"",
+  "keyword": "When "
+});
+formatter.step({
+  "name": "Click on My Account link",
+  "keyword": "And "
+});
+formatter.step({
+  "name": "Send registered username as \"\u003cusername\u003e\" and password as \"\u003cpassword\u003e\"",
+  "keyword": "And "
+});
+formatter.step({
+  "name": "Click on LOGIN button",
+  "keyword": "And "
+});
+formatter.step({
+  "name": "User must be successfully redirected to home page",
+  "keyword": "Then "
+});
+formatter.examples({
+  "name": "",
+  "description": "",
+  "keyword": "Examples",
+  "rows": [
+    {
+      "cells": [
+        "username",
+        "password"
+      ]
+    },
+    {
+      "cells": [
+        "rahaoui.bilalma",
+        "invalid.password"
+      ]
+    },
+    {
+      "cells": [
+        "invalid.email",
+        "Testing^^password@12456#"
+      ]
+    },
+    {
+      "cells": [
+        "invalid.email",
+        "invalid.password"
+      ]
+    }
+  ]
+});
 formatter.scenario({
   "name": "Login with valid username and password",
   "description": "",
-  "keyword": "Scenario"
+  "keyword": "Scenario Outline"
 });
 formatter.before({
   "status": "passed"
@@ -43,7 +103,7 @@ formatter.result({
   "status": "passed"
 });
 formatter.step({
-  "name": "Send registered username as \"rahaoui.bilalma\" and password as \"Testing^^password@12456#\"",
+  "name": "Send registered username as \"rahaoui.bilalma\" and password as \"invalid.password\"",
   "keyword": "And "
 });
 formatter.match({
@@ -70,7 +130,152 @@ formatter.match({
   "location": "LoginSteps.userMustBeSuccessfullyRedirectedToHomePage()"
 });
 formatter.result({
+  "error_message": "java.lang.AssertionError: Test failed because text is not visible or not match\r\n\tat org.junit.Assert.fail(Assert.java:88)\r\n\tat org.junit.Assert.assertTrue(Assert.java:41)\r\n\tat Steps.LoginSteps.userMustBeSuccessfullyRedirectedToHomePage(LoginSteps.java:68)\r\n\tat ✽.User must be successfully redirected to home page(file:src/test/java/Feature/Login.feature:10)\r\n",
+  "status": "failed"
+});
+formatter.after({
   "status": "passed"
+});
+formatter.scenario({
+  "name": "Login with valid username and password",
+  "description": "",
+  "keyword": "Scenario Outline"
+});
+formatter.before({
+  "status": "passed"
+});
+formatter.step({
+  "name": "Open browser",
+  "keyword": "Given "
+});
+formatter.match({
+  "location": "LoginSteps.openBrowser()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "Enter the url \"http://practice.automationtesting.in/\"",
+  "keyword": "When "
+});
+formatter.match({
+  "location": "LoginSteps.enterTheUrl(String)"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "Click on My Account link",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "LoginSteps.clickOnMyAccountLink()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "Send registered username as \"invalid.email\" and password as \"Testing^^password@12456#\"",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "LoginSteps.sendRegisteredUsernameAsAndPasswordAs(String,String)"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "Click on LOGIN button",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "LoginSteps.clickOnLOGINButton()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "User must be successfully redirected to home page",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "LoginSteps.userMustBeSuccessfullyRedirectedToHomePage()"
+});
+formatter.result({
+  "error_message": "java.lang.AssertionError: Test failed because text is not visible or not match\r\n\tat org.junit.Assert.fail(Assert.java:88)\r\n\tat org.junit.Assert.assertTrue(Assert.java:41)\r\n\tat Steps.LoginSteps.userMustBeSuccessfullyRedirectedToHomePage(LoginSteps.java:68)\r\n\tat ✽.User must be successfully redirected to home page(file:src/test/java/Feature/Login.feature:10)\r\n",
+  "status": "failed"
+});
+formatter.after({
+  "status": "passed"
+});
+formatter.scenario({
+  "name": "Login with valid username and password",
+  "description": "",
+  "keyword": "Scenario Outline"
+});
+formatter.before({
+  "status": "passed"
+});
+formatter.step({
+  "name": "Open browser",
+  "keyword": "Given "
+});
+formatter.match({
+  "location": "LoginSteps.openBrowser()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "Enter the url \"http://practice.automationtesting.in/\"",
+  "keyword": "When "
+});
+formatter.match({
+  "location": "LoginSteps.enterTheUrl(String)"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "Click on My Account link",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "LoginSteps.clickOnMyAccountLink()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "Send registered username as \"invalid.email\" and password as \"invalid.password\"",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "LoginSteps.sendRegisteredUsernameAsAndPasswordAs(String,String)"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "Click on LOGIN button",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "LoginSteps.clickOnLOGINButton()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "User must be successfully redirected to home page",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "LoginSteps.userMustBeSuccessfullyRedirectedToHomePage()"
+});
+formatter.result({
+  "error_message": "java.lang.AssertionError: Test failed because text is not visible or not match\r\n\tat org.junit.Assert.fail(Assert.java:88)\r\n\tat org.junit.Assert.assertTrue(Assert.java:41)\r\n\tat Steps.LoginSteps.userMustBeSuccessfullyRedirectedToHomePage(LoginSteps.java:68)\r\n\tat ✽.User must be successfully redirected to home page(file:src/test/java/Feature/Login.feature:10)\r\n",
+  "status": "failed"
 });
 formatter.after({
   "status": "passed"
