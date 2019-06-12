ed7c94cfa13adf1053ec3a54a1098d84995c2712
Bilal Rahaoui <47571557+BilalRahaoui@users.noreply.github.com>
2019-06-12 16:11:29 +0100

    New @CucumberOptions

--- src/test/java/TestRunner/TestRunner.java
+++ src/test/java/TestRunner/TestRunner.java
@@ -11,12 +11,12 @@ import org.junit.runner.RunWith;
         glue = "Steps", // to map feature file and step definition
         dryRun = false, /* default is false,if true , cucumber will only checks that every Step mentioned in the Feature File
                            have corresponding code written in Step Definition file or not*/
-        monochrome = true, // default is false,it's used to add monochrome to console 'Need this if you use Eclipse IDE'
+        monochrome = true,
         plugin = {
                 "pretty", // to generate report in console as written in feature file
-                "html:test-output/HTMLReport", // to genrate html report
-                "json:test-output/JSONReport/cucumber.json", // to genrate json file report
-                "junit:test-output/XMLReport/cucumber.xml" // to genrate xml file report
+                "html:test-output/HTMLReport", // to generate html report
+                "json:test-output/JSONReport/cucumber.json", // to generate json file report
+                "junit:test-output/XMLReport/cucumber.xml" // to generate xml file report
         },
         strict = true // default is false, if true, test will fail if step definition is undefined, else test will be ignored
 
